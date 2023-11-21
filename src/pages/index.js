d005d2c8dd5da3ec02df16a4a806c5117a2a24d9
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-11-21 14:31:30 +0300

    Update home page UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
-import hero from '../../public/assets/hero.png'
-// import hero from '../../public/assets/hero2.png'
+import hero from '../../public/assets/hero.png';
+
 
 export default function HomePage() {
   return <div className="grid-halves h-screen-navbar">
-    <div className="bg-teal border-right">
+    <div className="border-right">
       <div className="column-padding">
         <div className="tablet-centered">
           <div className="content-grid home-hero">
-            <h1>The most <br /> epic products.</h1>
-            <p className="section-subtitle">All the most epic things in the internt, all in one place.</p>
+            <h1>Monthly newsletters, <br/> made simple.</h1>
+            <p className="section-subtitle">Today&apos;s headlines make today&apos;s head lines.</p>
             <Link href="/products" className="large-buttton">
-              <div className="large-button-text">Explore Products</div>
+              <div className="large-button-text">Explore more</div>
             </Link>
           </div>
         </div>
       </div>
     </div>
-    <div className="bg-salmon">
+    <div >
+
       <div className="centered">
         <div className="callout-wrap">
           <Image src={hero} placeholder="empty" className="callout-image" alt="hero img"></Image>
-          {/* <Image src={hero} placeholder="empty" className="callout-image" alt="hero img"></Image> */}
+
         </div>
       </div>
     </div>
