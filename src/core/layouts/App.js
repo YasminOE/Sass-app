63c5282a2fd259eb1d69426315d077a2438828eb
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-11-22 13:15:50 +0300

    Saving progrss : page transition wroking but not as wish

--- src/core/layouts/App.js
+++ src/core/layouts/App.js
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router'
 import Meta from 'src/core/components/Meta'
 import Navbar from "src/core/components/Navbar"
+import {motion , AnimatePresence} from "framer-motion"
+import Logo from '/src/core/components/Logo.js'
 
 const hideNavbarPages = ['/success', '/login']
 
@@ -9,9 +11,39 @@ export default function AppLayout({children}) {
 
   const hideNavbar = hideNavbarPages.includes(router.asPath)
   return <>
-    <Meta />
+
+    {/* <AnimatePresence mood="wait"> */}
+     {/* <motion.div key={router.pathname}> */}
+     <Meta />
     {hideNavbar ? null : <Navbar/>}
     {children}
+  {/* </motion.div> */}
+
+  <motion.div
+    className='slide-in'
+    initial={{scaleY: 0}}
+    animate={{scaleY: 0}}
+    exit={{scaleY: 1}}
+    transition={{ duration: 3, ease: [0.7, 0, 0.3, 1]}}
+    // transition={{ duration: 1, ease: [0.7, 0, 0.3, 1]}}
+    key='slide-in'
+  >
+    
+ 
+  </motion.div>
+
+  <motion.div
+        className='slide-out'
+        initial={{scaleY: 1}}
+        animate={{scaleY: 0}}
+        exit={{scaleY: 0}}
+        transition={{ duration: 2.6, ease: [0.22, 1, 0.36, 1]}}
+        key='slide-out'
+  >
+     
+  </motion.div>
+
+  {/* </AnimatePresence> */}
 
   </>
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -26,48 +26,21 @@ export default function App({ Component, pageProps }) {
    );
   return (
 
+   <AnimatePresence mood="wait"> 
     <SessionContextProvider
   // supabaseClient={supabaseClient}
   supabaseClient={supabaseClient}
     initialSession={pageProps.initialSession}
   >
-     <AnimatePresence mood="wait">
      <motion.div key={router.pathname}>
     <AppLayout >
       <Component {...pageProps} />
     </AppLayout>
   </motion.div>
 
-  <motion.div
-    className='slide-in'
-    initial={{scaleY: 0}}
-    animate={{scaleY: 0}}
-    exit={{scaleY: 1}}
-    transition={{ duration: 0.6, ease: [0.7, 0, 0.3, 1]}}
-    // transition={{ duration: 1, ease: [0.7, 0, 0.3, 1]}}
-    key='slide-in'
-  >
-       <div className="centered">
-      <Logo />
-    </div>
- 
-  </motion.div>
 
-  <motion.div
-        className='slide-out'
-        initial={{scaleY: 1}}
-        animate={{scaleY: 0}}
-        exit={{scaleY: 1}}
-        transition={{ duration: 0.6, ease: [0.22, 1, 0.36, 1]}}
-        key='slide-out'
-  >
-        <div className="centered">
-      <Logo />
-    </div>
-  </motion.div>
-
-  </AnimatePresence>
 
   </SessionContextProvider>
+   </AnimatePresence>
   ) 
 }
