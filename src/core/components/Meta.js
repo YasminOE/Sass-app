6e68768ea17652e167ee141a4e4d57a0e27d90e8
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-12-07 10:15:42 +0300

    Change the domain name

--- src/core/components/Meta.js
+++ src/core/components/Meta.js
@@ -4,7 +4,9 @@ export default function Meta() {
       <title>Paperboy</title>
       <meta
         name="description"
-        content="Monthly newsletter reads, made simple!"
+        content=" Paperboy. where newsletter meets simplicity, and a clutter-free home because your inbox deserves a breath of fresh air! 📰✨"
+        // content=" Paperboy empowers you to curate your news experience. Picture a personalized monthly compilation that distills the latest and most relevant updates, delivering a curated selection directly to your virtual doorstep."
+        // content="Monthly newsletter reads, made simple right at your device"
       />
     </>
   );
