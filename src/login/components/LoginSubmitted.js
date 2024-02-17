74565438aebce4cb7e05403bcf4b0c820b199e34
Yasmin Elmahdi <elmahdiyasmin@.com>
2024-02-17 21:30:32 +0300

    copy change

--- src/login/components/LoginSubmitted.js
+++ src/login/components/LoginSubmitted.js
@@ -2,7 +2,7 @@ export default function LoginSubmitted({ submitted }) {
   return(
     <div className="content-grid home-hero">
       <h1>Link sent!</h1>
-      <p>Please check the email sent to ({submitted}) to complete logging in with a Magic log-in Link.</p>
+      <p>Please check the email sent to ({submitted}) to complete logging in with a Magic Login Link.</p>
     </div>
   )
 }
